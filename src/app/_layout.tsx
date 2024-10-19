df59ea46ed298c8121dd8a4bea4ead6efc604d6b
B Karthik <bangikarthik7@gmail.com>
2024-10-19 18:58:41 +0530

    some basic start

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack>
+    <Stack.Screen name="(shop)" options={{headerShown: true, title: 'Shop'}} />
+    <Stack.Screen name="categories" options={{headerShown: true, title: 'Categories'}} />
+    <Stack.Screen name="products" options={{headerShown: true, title: 'Products'}} />
+    <Stack.Screen name="cart" options={{presentation: 'modal', title: 'Shopping Cart'}} />
+    <Stack.Screen name="auth" options={{headerShown: true}} />
+  </Stack>;
+}
