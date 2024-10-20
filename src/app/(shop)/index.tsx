d020d52cfa356f73218d107b08ef6b76dcb5a8e3
B Karthik <bangikarthik7@gmail.com>
2024-10-20 15:38:50 +0530

    feat: Product List

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,13 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { PRODUCTS } from "../../../assets/products";
+import ProductListItem from "../components/product-list-item";
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  flatListColumn: {
+    justifyContent: 'space-between',
+  },
+  flatListContent: {
+    paddingBottom:20,
+    flexGrow: 1,
+  },
+});
